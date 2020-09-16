57baed7a1835046d7d7731fb109a4078afe6d9aa
Justin_Webb <Webb.n.justin@gmail.com>
2020-09-16 10:27:39 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
-# your code goes here
+
+
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness 
+    
+    def initialize(name)
+        @name = name 
+        @bank_account = 25
+        @happiness = 8
+
+    end
+
+    def happiness=(happiness)
+        if happiness == 0..10
+            self.happiness = happiness
+        elsif
+            happiness = 0..10
+        end
+    end
+
+
+end
